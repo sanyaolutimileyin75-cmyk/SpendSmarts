9ea4e20e5291b468bb55a7e6ec7a5293b7864ac4
Sanyaolu Timilehin <sanyaolutimileyin75@gmail.com>
2026-07-01 15:51:04 +0100

    added a new model to this project and also fixed the error in the database

---
+++ SpendSmart2/Models/Helpers/CurrencyHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace SpendSmart2.Models.Helpers
+{
+    public static class CurrencyHelper
+    {
+        public static string FormatCurrency(decimal amount)
+        {
+            if (amount >= 1000000000)
+                return $"₦{amount / 1000000000m:0.#}B";
+
+            if (amount >= 1000000)
+                return $"₦{amount / 1000000m:0.#}M";
+
+            if (amount >= 1000)
+                return $"₦{amount / 1000m:0.#}K";
+
+            return $"₦{amount:N2}";
+        }
+    }
+}
